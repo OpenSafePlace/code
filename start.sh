ba0845df50800b0699c3ee55538a5797d9818bec
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-23 18:18:56 +0300

    feat: added a new launch type for the container

--- start.sh
+++ start.sh
@@ -4,8 +4,13 @@
 
 host=$(uname -n)
 port=${1:-9888}
+container=${2:-0}
 
-(sh ./Core/scripts/code-server.sh --launch --host=$host --port=$port --without-connection-token) > /dev/null 2>&1 &
+[ "$container" == "0" ] && {
+(NODE_ENV=development VSCODE_DEV=1 node ./Core/scripts/code-server.js --launch --host=$host --port=$port --without-connection-token > /dev/null 2>&1 &)
 
 info "Ссылка -- http://$host:${port}"
+} ||
+(NODE_ENV=development VSCODE_DEV=1 cpulimit --limit=\$global__project_power --lazy --include-children node ./Core/scripts/code-server.js --launch --host=$host --port=$port --without-connection-token > /dev/null 2>&1 &)
+
 info "Редактор кода запущен"
