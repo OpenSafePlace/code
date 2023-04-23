ae0f72a00427e4bbf9df8c5e4bf587df75eb3c4a
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-23 18:41:20 +0300

    fix: new launch type for the container

--- start.sh
+++ start.sh
@@ -11,6 +11,6 @@ container=${2:-0}
 
 info "Ссылка -- http://$host:${port}"
 } ||
-(NODE_ENV=development VSCODE_DEV=1 cpulimit --limit=\$global__project_power --lazy --include-children node ./Core/scripts/code-server.js --launch --host=$host --port=$port --without-connection-token > /dev/null 2>&1 &)
+(NODE_ENV=development VSCODE_DEV=1 cpulimit --limit=$global__project_power --lazy --include-children node ./Core/scripts/code-server.js --launch --host=$host --port=$port --without-connection-token > /dev/null 2>&1 &)
 
 info "Редактор кода запущен"
