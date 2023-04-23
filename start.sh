210245ada0bf73bcf65f82cc72cda278b0e71963
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-23 12:00:11 +0300

    fix: removed connection token

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 
 port=${1:-9888}
 
-(sh ./Core/scripts/code-server.sh --launch --host=localhost --port=$port) > /dev/null 2>&1 &
+(sh ./Core/scripts/code-server.sh --launch --host=localhost --port=$port --without-connection-token) > /dev/null 2>&1 &
 
 info "Ссылка -- http://localhost:${port}"
 info "Редактор кода запущен"
