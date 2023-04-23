cd2b91084c2932bee05f32af46b4b98774738d3a
Artur Shkadzinski <real-xihnik@yandex.by>
2023-04-23 12:05:59 +0300

    feat: added the ability to use a different hostname

--- start.sh
+++ start.sh
@@ -2,9 +2,10 @@
 
 . ./.Bricks/Dependencies/message-types-set.sh
 
+host=$(uname -n)
 port=${1:-9888}
 
-(sh ./Core/scripts/code-server.sh --launch --host=localhost --port=$port --without-connection-token) > /dev/null 2>&1 &
+(sh ./Core/scripts/code-server.sh --launch --host=$host --port=$port --without-connection-token) > /dev/null 2>&1 &
 
-info "Ссылка -- http://localhost:${port}"
+info "Ссылка -- http://$host:${port}"
 info "Редактор кода запущен"
